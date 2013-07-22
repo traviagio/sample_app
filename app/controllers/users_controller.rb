f64367bf41ab5c35a6494798eed701d91b4d6c7d
Georgi <georgiev.georgio@gmail.com>
2013-07-22 15:42:42 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new
+  end
+
+ def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to our Subscription based language travel club that allows members to travel to 50+ locations and learn French, Spanish, Italian and Mandarin at their convenience."
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
